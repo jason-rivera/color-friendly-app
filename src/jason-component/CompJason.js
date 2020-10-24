81eb42f3f816076efae1549f931dc933ad78b252
Jason Rivera <jp_rivera_@hotmail.com>
2020-10-24 11:48:50 -0700

    added jason's component

---
+++ src/jason-component/CompJason.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./App.css";
+
+const compJason = () => {
+  return (
+    <div class="button">
+      'Go' button
+    </div>
+  )
+}
+
+export default component-1
