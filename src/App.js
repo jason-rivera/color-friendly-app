dcbd34b37ce8b285834a60522d71b1e80f693641
Jason Rivera <jp_rivera_@hotmail.com>
2020-10-24 08:46:04 -0700

    jason

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1>Hello World!</h1>
+          <h2>jason</h2>
           <a
             className="App-link"
             href="https://reactjs.org"
